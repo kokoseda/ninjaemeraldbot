695897212dac297fe9b623eacc7d97cd88e61146
kokoseda <kokoseda22@yahoo.com>
2018-10-10 21:39:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -217,7 +217,7 @@ client.on('message', message => {
   .setAuthor(message.author.username)
   .setFooter(`Fr3onGamer#0440`, 'https://images-ext-2.discordapp.net/external/X9SanEG0s7Dtv3krTgy-kod_fj6JRVJ2AG4JryCiiz0/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/375761288518828042/fffa31c797e88cf059dd6db424ff456a.png?width=80&height=80')
   .setColor("#000000")
-  .addField("Done | تــــم" , ":envelope: | :sleuth_or_spy::skin-tone-3:Check Your DM :white_check_mark:")
+  .addField("Done | تــــم" , ":envelope: | :sleuth_or_spy: :fast_forward: Check Your DM :white_check_mark: ")
   
   
   
