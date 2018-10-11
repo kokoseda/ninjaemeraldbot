36e48f77094bef752875e63a1892f59b7a2aeb4e
kokoseda <kokoseda22@yahoo.com>
2018-10-11 15:14:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Discord.js bot
 const Discord = require('discord.js');
 const { Client, Util } = require('discord.js');
-const client = new Client();
+const client = new Discord.Client();
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
@@ -9101,11 +9101,9 @@ client.on('message', message => {
 
 
 
-
-const client = new Discord.Client();
+;
 let ar = JSON.parse(fs.readFileSync(`./AutoRole.json`, `utf8`))
 
-const prefix = "$";//البريفكس
 
 client.on('guildMemberAdd', member => {
   if(!ar[member.guild.id]) ar[member.guild.id] = {
