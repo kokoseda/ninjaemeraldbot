1ce74e1460d19474d2f8c456d3491c5f764dc1f4
kokoseda <kokoseda22@yahoo.com>
2018-10-11 13:36:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9044,7 +9044,27 @@ hastebin(`${array.slice(0, 30).join('\n')}`, 'txt').then(l => {
 
 
 
-
+ client.on('message', function(message) {
+    		if(!message.member.hasPermission('ADMINISTRATOR'))
+            let command = message.content.split(" ")[0];
+        if(message.content.includes('discord.gg')){
+        message.reply (' ')
+           if(!message.channel.guild) return message.reply('** This command only for servers**');
+     message.member.addRole(message.guild.roles.find('name', 'Muted')); 
+    const embed500 = new Discord.RichEmbed()
+      .setTitle("❌ | تمت معاقبتك")
+            .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
+      .addField(`by`,`shyboy_05`)
+            .setColor("c91616")
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setAuthor(message.author.username, message.author.avatarURL) 
+        .setFooter(`${message.guild.name} Server`)
+     message.channel.send(embed500) 
+    
+        
+    }
+    }
+})
 
 
 
